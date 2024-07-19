ff8fd70f070fc7090a838ce891a04752165f4eca
Lucas K <lucas@lkellar.org>
2024-07-19 01:31:35 -0400

    stripped solution for part 1

--- Day 24/main.swift
+++ Day 24/main.swift
@@ -3,16 +3,8 @@
 //  Day 24
 //
 //  Created by Lucas Kellar on 7/10/24.
-//
-// Implements the Bentley-Ottmann Algorithm
-// https://en.wikipedia.org/wiki/Bentley%E2%80%93Ottmann_algorithm
-
-// TODO. the stupid search algorithim needs work
-// TODO BRUTE FORC
 
 import Foundation
-import HeapModule
-import OrderedCollections
 
 let path = CommandLine.arguments[1]
 let MIN_DIM = Double(CommandLine.arguments[2])!
@@ -52,7 +44,6 @@ struct Coord: Hashable {
         }
         return true
     }
-
     
     var description: String { return "(\(x), \(y), \(z))" }
 }
@@ -128,12 +119,7 @@ func findNextBoundingBoxInterception(start: Coord, deltas: Coord, considerZ: Boo
     }
 }
 
-// comparable based on doing a comparison based on an x coordinate
-protocol DynamicallyComparable: Equatable {
-    func getMomentForX(_ x: Double) -> HailstoneMoment
-}
-
-struct Hailstone: DynamicallyComparable {
+struct Hailstone {
     let start: Coord
     let end: Coord
     let slope: Double
@@ -177,13 +163,6 @@ struct Hailstone: DynamicallyComparable {
         // lines don't cross
         return nil
     }
-    
-    func getMomentForX(_ x: Double) -> HailstoneMoment {
-        let deltaX = x - start.x
-        let y = (deltaX * slope) + start.y
-        return HailstoneMoment(y: y, slope: slope)
-    }
-
     init(_ str: String) throws {
         let splits = str.split(separator: "@")
 
@@ -195,7 +174,7 @@ struct Hailstone: DynamicallyComparable {
         precondition(deltas.y != 0, "DeltaY cannot be zero")
 
         let starts = splits[0].trimmingCharacters(in: .whitespaces).split(separator: ",").map { Double($0.trimmingCharacters(in: .whitespaces)) }
-        var origin = Coord(x: starts[0]!, y: starts[1]!, z: starts[2]!)
+        let origin = Coord(x: starts[0]!, y: starts[1]!, z: starts[2]!)
         var localStart = origin
         while !localStart.withinBoundingBox {
             guard let next = findNextBoundingBoxInterception(start: localStart, deltas: deltas) else {
@@ -220,256 +199,29 @@ struct Hailstone: DynamicallyComparable {
     }
 }
 
-// from a left to right perspective
-enum LineSide {
-    case front
-    case back
-    case middle
-    
-    var description: String {
-        switch self {
-        case .front:
-            return "front"
-        case .back:
-            return "back"
-        case .middle:
-            return "middle"
-        }
-    }
-}
-
-struct HailstoneMoment: Comparable {
-    let y: Double
-    let slope: Double
-    
-    static func < (lhs: HailstoneMoment, rhs: HailstoneMoment) -> Bool {
-        let diff = lhs.y - rhs.y
-        if abs(diff) < 0.00000001 {
-            return lhs.slope < rhs.slope
-        } else if diff < 0 {
-            return true
-        } else if diff > 0 {
-            return false
-        } else {
-            return lhs.slope < rhs.slope
-        }
-    }
-    
-    static func == (lhs: HailstoneMoment, rhs: HailstoneMoment) -> Bool {
-        return lhs.y == rhs.y && lhs.slope == rhs.slope
-    }
-    
-    var description: String {
-        return "(y: \(y) | slope: \(slope))"
-    }
-}
-
-struct Endpoint: Comparable {
-    let coord: Coord
-    let end: LineSide
-    let stone: Hailstone
-    
-    static func == (lhs: Endpoint, rhs: Endpoint) -> Bool {
-        return lhs.coord.x == rhs.coord.x && lhs.coord.y == rhs.coord.y
-    }
-
-    // return lowest x, break ties with lowest y
-    static func < (lhs: Endpoint, rhs: Endpoint) -> Bool {
-        let diff = lhs.coord.x - rhs.coord.x
-        if diff < 0 {
-            return true
-        } else if diff > 0 {
-            return false
-        } else {
-            return lhs.coord.y < rhs.coord.y
-        }
-    }
-}
-
 let lines: [Hailstone] = contents.split(whereSeparator: \.isNewline).reduce(into: []) { result, next in
     if let stone = try? Hailstone(String(next)) {
         result.append(stone)
     }
 }
 
-struct Calculamator {
-    var priorityQueue: Heap<Endpoint> = []
-    var tree: [Hailstone] = []
-    var currentX: Double = MIN_DIM
-    var intersectionCount: Int = 0
-    
-    mutating func calculmate() -> Int {
-        while let curr = priorityQueue.popMin() {
-            //print("Popping a \(curr.end) at \(curr.coord.x)")
-            process(endpoint: curr)
-            //print()
-        }
-        return intersectionCount
-    }
-    
-    mutating func loadInitialEndpoints() {
-        for stone in lines {
-            if stone.end.x < stone.start.x {
-                priorityQueue.insert(Endpoint(coord: stone.end, end: .front, stone: stone))
-                priorityQueue.insert(Endpoint(coord: stone.start, end: .back, stone: stone))
-            } else {
-                priorityQueue.insert(Endpoint(coord: stone.end, end: .back, stone: stone))
-                priorityQueue.insert(Endpoint(coord: stone.start, end: .front, stone: stone))
+func computeIntersections() -> Int {
+    var intersections = 0
+    for outer in 0..<lines.count {
+        for inner in (outer + 1)..<lines.count {
+            if lines[outer].intersectsStone(other: lines[inner]) != nil {
+                intersections += 1
             }
         }
     }
-    
-    // returns index of tree where the stone there is ordered HIGHER or equal to than the stone at the current X
-    func findTreeIndexForHailstone(_ stone: Hailstone) -> Int {
-        /*print("Current X: \(currentX)")
-        tree.map {print($0.getYForX(currentX))}
-        print()*/
-        var left = 0
-        var right = tree.count - 1
-        let stoneMoment = stone.getMomentForX(currentX)
-        while left <= right {
-            let m = (left + right) / 2
-            let treeMoment = tree[m].getMomentForX(currentX)
-            
-            if tree[m] == stone {
-                return m
-            } else if treeMoment < stoneMoment {
-                if tree.count > m + 1 {
-                    if stoneMoment < tree[m + 1].getMomentForX(currentX) {
-                        return m
-                    } else {
-                        left = m + 1
-                    }
-                } else {
-                    return tree.count
-                }
-            } else {
-                right = m - 1
-            }
-        }
-        if right < 0 {
-            return 0
-        }
-        // everything is lower than stone
-        return tree.count
-    }
-    
-    func pointBehindSweepLine(_ point: Coord) -> Bool {
-        return point.x < currentX
-    }
-    
-    mutating func insertIntersection(_ endpoint: Endpoint) {
-        guard endpoint.end == .middle else {
-            print("Intersection must be type middle")
-            return
-        }
-        // if intersection behind sweep line, ignore
-        guard !pointBehindSweepLine(endpoint.coord) else {
-            return
-        }
-        priorityQueue.insert(endpoint)
-    }
-    
-    func validateTree() {
-        guard tree.count >= 2 else {
-            return
-        }
-        for index in 0..<(tree.count - 1) {
-            let lower = tree[index].getMomentForX(currentX)
-            let upper = tree[index + 1].getMomentForX(currentX)
-            
-            guard lower < upper else {
-                print("AAAAH: \(index): \(lower) | \(index + 1): \(upper)")
-                exit(1)
-            }
-        }
-    }
-    
-    func compute() -> Int {
-        var intersections = 0
-        for outer in 0..<lines.count {
-            for inner in (outer + 1)..<lines.count {
-                if lines[outer].intersectsStone(other: lines[inner]) != nil {
-                    intersections += 1
-                }
-            }
-        }
-        return intersections
-        //print(intersections)
-    }
-
-    
-    mutating func process(endpoint: Endpoint) {
-        //validateTree()
-        let end = endpoint.end
-        let coord = endpoint.coord
-        let stone = endpoint.stone
-        if end == .front {
-            //print("Front, nice!", coord)
-            currentX = coord.x
-            let index = findTreeIndexForHailstone(stone)
-            tree.insert(stone, at: index)
-            if index < (tree.count - 1) {
-                let above = tree[index + 1]
-                if let intersectionPoint = stone.intersectsStone(other: above) {
-                    //print("ABOVE MY GOD", intersectionPoint, above, stone)
-                    insertIntersection(Endpoint(coord: intersectionPoint, end: .middle, stone: stone))
-                }
-            }
-            if index > 0 {
-                let below = tree[index - 1]
-                if let intersectionPoint = stone.intersectsStone(other: below) {
-                    //print("OH MY GOD", intersectionPoint)
-                    insertIntersection(Endpoint(coord: intersectionPoint, end: .middle, stone: below))
-                }
-            }
-        } else if end == .back {
-            let index = findTreeIndexForHailstone(stone)
-            guard index < tree.count else {
-                print("Somehow it's not there")
-                exit(1)
-            }
-            tree.remove(at: index)
-            currentX = coord.x
-            if tree.count > index && index > 0 {
-                if let intersectionPoint = tree[index].intersectsStone(other: tree[index - 1]) {
-                    //print("OH MY chode", intersectionPoint)
-                    insertIntersection(Endpoint(coord: intersectionPoint, end: .middle, stone: tree[index - 1]))
-                }
-            }
-        } else if end == .middle {
-            intersectionCount += 1
-            let index = findTreeIndexForHailstone(stone)
-            //print("Found one: \(index), \(index + 1)")
-            tree.swapAt(index, index + 1)
-            currentX = coord.x
-            if tree.count > index + 2 {
-                if let intersectionPoint = tree[index + 1].intersectsStone(other: tree[index + 2]) {
-                    //print("OH MY a la mode", intersectionPoint)
-                    insertIntersection(Endpoint(coord: intersectionPoint, end: .middle, stone: tree[index + 1]))
-                }
-            }
-            if index > 0 {
-                if let intersectionPoint = tree[index].intersectsStone(other: tree[index - 1]) {
-                    //print("OH MY whode", intersectionPoint)
-                    insertIntersection(Endpoint(coord: intersectionPoint, end: .middle, stone: tree[index - 1]))
-                }
-            }
-        } else {
-            print("Impossible")
-            exit(1)
-        }
-    }
+    return intersections
 }
-
 
 
 // PART_TWO handled within
 let clock = ContinuousClock()
 let elapsed = clock.measure {
-    // calc is slang for calculator
-    let calc = Calculamator()
-    let intersections = calc.compute()
+    let intersections = computeIntersections()
     print("\(intersections) Intersections")
 }
 print("Took \(elapsed)")
